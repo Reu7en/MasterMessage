67836109a1e36e1c6729894251c843144c1ea46b
Reu7en <baker.reuben@outlook.com>
2024-11-27 00:15:46 +0000

    Set up duplicate iOS app

---
+++ MasterMessage/MasterMessageiOSDuplicate/MasterMessageiOSDuplicateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterMessageiOSDuplicateApp.swift
+//  MasterMessageiOSDuplicate
+//
+//  Created by Reuben on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MasterMessageiOSDuplicateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ MasterMessage/MasterMessageiOSDuplicate/Views/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  MasterMessageiOSDuplicate
+//
+//  Created by Reuben on 27/11/2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    RootView()
+}
